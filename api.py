2a1e123a62d5d58864a4f6de928199e0f43d8542
Truong Nguyen <t.nguyen@innopolis.university>
2023-11-08 10:14:06 +0700

    reformatted and error handling the top_n_asset_performance function

--- api.py
+++ api.py
@@ -3,38 +3,47 @@ import os
 
 CENTIC_JWT_TOKEN = os.getenv("CENTIC_JWT_TOKEN")
 
-def top_n_asset_performance(num_asset:int, criteria:str, interval:int):
-    r"""
-    Intent: ask_onchain/top_n_asset_performance
-    Query top num_asset of tokens based on criteria over interval
-    Input:
-        num_asset
-        criteria: in ["marketCap", "tradingVolume", "tokenHealth", "tradingVolumeChangeRate"]
-        interval: by days
-    Output:
-        Everything is obvious except:
-        "asset": Key: UNIX timestamp according to the interval specification, Value: total amount of tokens hold as asset (in wallets across all chains supported).
-
-        "performance": Key, same as above, Value: percentage of total value increase/decrease w.r.t to the previous timestamp 
-        "dailyPAndL": Key, same as above, Value: Profit/Loss based on market price at the time, w.r.t to the previous timestamp
-        "cumulativePAndL": ...
-        "greatEvents": array of this token transfer of significant amount
-        "exchanges": arrray of this token amount over different exchanges
+def top_n_asset_performance(num_asset: int, criteria: str, interval: int):
     """
+    Queries top num_asset of tokens based on criteria over interval.
 
-    if criteria in ["marketCap", "tradingVolume", "tokenHealth", "tradingVolumeChangeRate"]:
-        pass
-    else:
-        raise
+    :param num_asset: Number of top-performing assets to return
+    :param criteria: Performance criteria (marketCap, tradingVolume, tokenHealth, tradingVolumeChangeRate)
+    :param interval: Time interval for performance, in days
+    :return: List of dictionaries containing asset performance data
+    """
+    
+    # Validate criteria
+    valid_criteria = {"marketCap", "tradingVolume", "tokenHealth", "tradingVolumeChangeRate"}
+    if criteria not in valid_criteria:
+        raise ValueError(f"Criteria must be one of {valid_criteria}")
 
-    asset_list_query_url = f"https://api-staging.centic.io/dev/v3/ranking/tokens?order=desc&orderBy={criteria}&pageSize={num_asset}&duration={interval}"
-    asset_list = requests.get(asset_list_query_url).json()['docs']
+    # Construct the URL to get the list of top assets
+    asset_list_query_url = (
+        f"https://api-staging.centic.io/dev/v3/ranking/tokens"
+        f"?order=desc&orderBy={criteria}&pageSize={num_asset}&duration={interval}"
+    )
 
-    for i in range(len(asset_list)):
-        asset_id = asset_list[i]['id']
+    # Fetch the asset list
+    try:
+        response = requests.get(asset_list_query_url)
+        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        asset_list = response.json().get('docs', [])
+    except requests.RequestException as e:
+        raise RuntimeError("Failed to fetch asset list") from e
+
+    # Fetch analytics for each asset and update the asset information
+    for asset in asset_list:
+        asset_id = asset.get('id')
         asset_analytics_query_url = f"https://api-staging.centic.io/dev/v3/common/analytics?id={asset_id}&type=token"
-        asset_analytics = requests.get(asset_analytics_query_url).json()
-        asset_list[i].update(asset_analytics)
+        
+        try:
+            analytics_response = requests.get(asset_analytics_query_url)
+            analytics_response.raise_for_status()  # Checks for HTTP errors
+            asset_analytics = analytics_response.json()
+            asset.update(asset_analytics)
+        except requests.RequestException as e:
+            raise RuntimeError(f"Failed to fetch analytics for asset {asset_id}") from e
 
     return asset_list
 
